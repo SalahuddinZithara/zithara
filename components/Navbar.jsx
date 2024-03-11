5e2b96a257e2e325931ac4772121a2af57bd2597
Salahuddin <ghazi@zithara.in>
2024-03-11 17:28:58 +0530

    fixed nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -229,7 +229,7 @@ const Sidebar = () => {
     <div
       className={` ${
         openMenu ? "h-screen" : " h-auto"
-      } duration-200 w-full flex flex-col shadow-md lg:justify-center py-2`}
+      } duration-200 w-full flex flex-col shadow-md lg:justify-center py-2.5`}
     >
       <div className=" w-full flex items-center justify-between px-5 md:px-8 lg:px-12 2xl:px-28">
         <span className=" cursor-pointer">
@@ -238,7 +238,7 @@ const Sidebar = () => {
             alt="logo"
             width={150}
             height={35}
-            className=" w-[150px] lg:w-[150px] lg:h-[35px]"
+            className=" w-[150px] lg:w-[150px] lg:h-[35px] object-cover"
             priority
           />
         </span>
